07d43b1be5194cc7886218d263807b7ab0e5f60e
Jürgen Altszeimer <juergenaltszeimer@t-online.de>
2018-09-08 15:51:48 +0200

    Enable loading of ARXML files
    - Minor visual adjustments like change of accent color teal

--- Source/ASR_Viewer/Viewer/ViewModels/MainViewModel.cs
+++ Source/ASR_Viewer/Viewer/ViewModels/MainViewModel.cs
@@ -1,8 +1,13 @@
-﻿using System.Collections.ObjectModel;
+﻿using System;
+using System.Collections.ObjectModel;
+using System.Windows.Input;
+using Microsoft.Win32;
+using Prism.Commands;
 using Prism.Mvvm;
 using Prism.Regions;
 using Reader;
 using Shared;
+using Shared.ASR;
 
 namespace Viewer.ViewModels
 {
@@ -10,9 +15,10 @@ namespace Viewer.ViewModels
     {
         private readonly IRegionManager _regionManager;
         private readonly IRegistrationService _registrationService;
-        private readonly AsrReader _reader = new AsrReader();
+        private Document _document;
 
-        private string _title = "ASR Viewer";
+        private const string BaseTitle = "ASR Viewer";
+        private string _title = BaseTitle;
         public string Title
         {
             get => _title;
@@ -21,6 +27,7 @@ namespace Viewer.ViewModels
 
         public ObservableCollection<IPlugin> Plugins { get; } = new ObservableCollection<IPlugin>();
 
+        public ICommand OpenCommand { get; }
 
         public MainViewModel(RegistrationService registrationService, IRegionManager regionManager)
         {
@@ -28,6 +35,22 @@ namespace Viewer.ViewModels
             _registrationService = registrationService;
 
             registrationService.NewRegistration += UpdatePluginList;
+
+            OpenCommand = new DelegateCommand(OpenNewDocument);
+        }
+
+        private void OpenNewDocument()
+        {
+            var dialog = new OpenFileDialog
+            {
+                Filter = "ARXML files (*.arxml)|*.arxml"
+            };
+
+            if (dialog.ShowDialog() != true)
+                return;
+
+            _document = new AsrReader().Read(dialog.FileName);
+            Title = BaseTitle + " | " + _document.Info.Path;
         }
 
         private void UpdatePluginList(object sender, NewRegistrationArgs args)
