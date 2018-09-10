233bd624b6a90675ee3f86c33852dc9940574e95
Jürgen Altszeimer <juergenaltszeimer@t-online.de>
2018-09-10 20:22:12 +0200

    Display ARXML file

--- Source/ASR_Viewer/BasicViews/ViewModels/OverviewViewModel.cs
+++ Source/ASR_Viewer/BasicViews/ViewModels/OverviewViewModel.cs
@@ -1,4 +1,5 @@
 ﻿using System.Collections.Generic;
+using System.Xml.Linq;
 using Prism.Mvvm;
 using Prism.Regions;
 using Shared.ASR;
@@ -7,7 +8,14 @@ namespace BasicViews.ViewModels
 {
     public class OverviewViewModel : BindableBase, INavigationAware
     {
-        public Document Document { get; set; }
+        private Document _document;
+
+        private XElement _arxml;
+        public XElement Arxml
+        {
+            get => _arxml;
+            set => SetProperty(ref _arxml, value);
+        }
 
         private int _packageCount;
         public int PackageCount
@@ -34,9 +42,10 @@ namespace BasicViews.ViewModels
         {
             if (!(navigationContext.Parameters["Document"] is Document doc)) return;
 
-            Document = doc;
+            _document = doc;
             Packages = new List<Package>(doc.Packages);
             PackageCount = _packages.Count;
+            Arxml = _document.Root;
         }
 
         public bool IsNavigationTarget(NavigationContext navigationContext)
