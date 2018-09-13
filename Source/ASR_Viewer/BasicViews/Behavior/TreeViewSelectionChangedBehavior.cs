4ba75bc6290d9c3c98cd2e2031c5a7fb3ec8be1c
Jürgen Altszeimer <juergenaltszeimer@t-online.de>
2018-09-13 11:37:33 +0200

    Enable fast visiting of selected AR-Package in ARXML view

---
+++ Source/ASR_Viewer/BasicViews/Behavior/TreeViewSelectionChangedBehavior.cs
@@ -0,0 +1,100 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+
+namespace BasicViews.Behavior
+{
+    /// <summary>
+    /// Source:
+    /// http://stackoverflow.com/questions/1034374/drag-and-drop-in-mvvm-with-scatterview
+    /// http://social.msdn.microsoft.com/Forums/de-DE/wpf/thread/21bed380-c485-44fb-8741-f9245524d0ae
+    /// 
+    /// Attached behaviour to implement the SelectionChanged command/event via delegate command binding or routed commands.
+    /// </summary>
+    public static class TreeViewSelectionChangedBehavior
+    {
+        /// <summary>
+        /// Field of attached ICommand property
+        /// </summary>
+        private static readonly DependencyProperty ChangedCommandProperty = DependencyProperty.RegisterAttached(
+            "ChangedCommand",
+            typeof(ICommand),
+            typeof(TreeViewSelectionChangedBehavior),
+            new PropertyMetadata(null, OnSelectionChangedCommandChange));
+
+        /// <summary>
+        /// Setter method of the attached ChangedCommand <seealso cref="ICommand"/> property
+        /// </summary>
+        /// <param name="source"></param>
+        /// <param name="value"></param>
+        public static void SetChangedCommand(DependencyObject source, ICommand value)
+        {
+            source.SetValue(ChangedCommandProperty, value);
+        }
+
+        /// <summary>
+        /// Getter method of the attached ChangedCommand <seealso cref="ICommand"/> property
+        /// </summary>
+        /// <param name="source"></param>
+        /// <returns></returns>
+        public static ICommand GetChangedCommand(DependencyObject source)
+        {
+            return (ICommand)source.GetValue(ChangedCommandProperty);
+        }
+
+        /// <summary>
+        /// This method is hooked in the definition of the <seealso cref="ChangedCommandProperty"/>.
+        /// It is called whenever the attached property changes - in our case the event of binding
+        /// and unbinding the property to a sink is what we are looking for.
+        /// </summary>
+        /// <param name="d"></param>
+        /// <param name="e"></param>
+        private static void OnSelectionChangedCommandChange(DependencyObject d, DependencyPropertyChangedEventArgs e)
+        {
+            if (d is TreeView uiElement)
+            {
+                uiElement.SelectedItemChanged -= Selection_Changed;
+
+                if (e.NewValue is ICommand)
+                {
+                    // the property is attached so we attach the Drop event handler
+                    uiElement.SelectedItemChanged += Selection_Changed;
+                }
+            }
+        }
+
+        /// <summary>
+        /// This method is called when the selection changed event occurs. The sender should be the control
+        /// on which this behaviour is attached - so we convert the sender into a <seealso cref="UIElement"/>
+        /// and receive the Command through the <seealso cref="GetChangedCommand"/> getter listed above.
+        /// 
+        /// The <paramref name="e"/> parameter contains the standard EventArgs data,
+        /// which is unpacked and reales upon the bound command.
+        /// 
+        /// This implementation supports binding of delegate commands and routed commands.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private static void Selection_Changed(object sender, RoutedPropertyChangedEventArgs<object> e)
+        {
+            // Sanity check just in case this was somehow send by something else
+            if (!(sender is TreeView uiElement))
+                return;
+
+            var changedCommand = GetChangedCommand(uiElement);
+
+            // There may not be a command bound to this after all
+            switch (changedCommand)
+            {
+                case null:
+                    return;
+                case RoutedCommand _:
+                    (changedCommand as RoutedCommand)?.Execute(e.NewValue, uiElement);
+                    break;
+                default:
+                    changedCommand.Execute(e.NewValue);
+                    break;
+            }
+        }
+    }
+}
--- Source/ASR_Viewer/BasicViews/ViewModels/OverviewViewModel.cs
+++ Source/ASR_Viewer/BasicViews/ViewModels/OverviewViewModel.cs
@@ -1,5 +1,5 @@
 ﻿using System.Collections.Generic;
-using System.Xml.Linq;
+using System.Collections.ObjectModel;
 using Prism.Mvvm;
 using Prism.Regions;
 using Shared.ASR;
@@ -10,11 +10,22 @@ namespace BasicViews.ViewModels
     {
         private Document _document;
 
-        private XElement _arxml;
-        public XElement Arxml
+        private ReadOnlyCollection<XElementViewModel> _root;
+        public ReadOnlyCollection<XElementViewModel> Root
         {
-            get => _arxml;
-            set => SetProperty(ref _arxml, value);
+            get => _root;
+            set => SetProperty(ref _root, value);
+        }
+
+        private object[] _selectPathItem;
+        public object[] SelectPathItem
+        {
+            get => _selectPathItem;
+            set
+            {
+                if (_selectPathItem != value)
+                    SetProperty(ref _selectPathItem, value);
+            }
         }
 
         private int _packageCount;
@@ -35,8 +46,62 @@ namespace BasicViews.ViewModels
         public Package SelectedPackage
         {
             get => _selectedPackage;
-            set => SetProperty(ref _selectedPackage, value);
+            set
+            {
+                SetProperty(ref _selectedPackage, value);
+                SelectPackageElement(value.Uuid);
+            }
         }
+
+        private void SelectPackageElement(string uuid)
+        {
+            var package = FindPackageElement(uuid);
+
+            if (package == null)
+                return;
+
+            var elements = Root[0].Elements;
+            elements[0].IsExpanded = false;
+
+            foreach (var element in elements[1].Elements)
+            {
+                element.IsExpanded = false;
+            }
+
+            SelectPathItem = GetPathFromRootTo(package);
+            package.IsExpanded = true;
+            package.IsSelected = true;
+        }
+
+        private XElementViewModel FindPackageElement(string uuid)
+        {
+            var package = Root[0].Element("AR-PACKAGES");
+            foreach (var element in package.Elements)
+            {
+                var attribute = element.Attribute("UUID");
+                if (attribute != null && attribute.Value == uuid)
+                    return element;
+            }
+
+            return null;
+        }
+
+        private static XElementViewModel[] GetPathFromRootTo(XElementViewModel element)
+        {
+            var path = new List<XElementViewModel>{element};
+
+            while (element.Parent != null)
+            {
+                path.Add(element.Parent);
+                element = element.Parent;
+            }
+
+            path.Reverse();
+
+            return path.ToArray();
+        }
+
+        #region INavigationAware
 
         public void OnNavigatedTo(NavigationContext navigationContext)
         {
@@ -44,8 +109,12 @@ namespace BasicViews.ViewModels
 
             _document = doc;
             Packages = new List<Package>(doc.Packages);
-            PackageCount = _packages.Count;
-            Arxml = _document.Root;
+            PackageCount = Packages.Count;
+            Root = new ReadOnlyCollection<XElementViewModel>(
+                new[]
+                {
+                    new XElementViewModel(_document.Root)
+                });
         }
 
         public bool IsNavigationTarget(NavigationContext navigationContext)
@@ -56,5 +125,7 @@ namespace BasicViews.ViewModels
         public void OnNavigatedFrom(NavigationContext navigationContext)
         {
         }
+
+        #endregion
     }
 }
